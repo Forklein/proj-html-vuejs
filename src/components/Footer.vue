7a4eabdc8ed27498b4f0515ba9cea1e4316bedaa
Forklein <giuseppepisani2@gmail.com>
2021-09-17 11:27:56 +0200

    End of footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,8 +2,41 @@
   <footer>
     <div class="container">
       <div class="row">
-        <div class="col-6"></div>
-        <div class="col-6"></div>
+        <div class="col-6 p-5">
+          <p class="fw-bold text-white">Address</p>
+          <p class="text-secondary">{{ address.place }}</p>
+          <p class="text-secondary">{{ address.hour }}</p>
+          <p class="text-secondary">{{ address.email }}</p>
+          <div class="row">
+            <div class="col d-flex">
+              <i class="fab fa-facebook-square fa-2x pe-2 text-secondary"></i>
+              <i class="fab fa-twitter fa-2x px-2 text-secondary"></i>
+              <i class="fab fa-instagram fa-2x px-2 text-secondary"></i>
+              <i class="fab fa-linkedin fa-2x px-2 text-secondary"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col-6 p-5">
+          <div class="row">
+            <div class="col">
+              <p class="fw-bold text-white">Explore</p>
+              <p v-for="text in explore" class="text-secondary" :key="text">
+                {{ text }}
+              </p>
+            </div>
+            <div class="col">
+              <p class="fw-bold text-white">Information</p>
+              <p v-for="text in information" class="text-secondary" :key="text">
+                {{ text }}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row text-center text-secondary mt-3">
+        <div class="col">
+          <p class="fw-bold">© 2020 Maxcoach.All Rights Reserved</p>
+        </div>
       </div>
     </div>
   </footer>
@@ -40,7 +73,6 @@ export default {
 
 <style scoped lang="scss">
 footer {
-  height: 500px;
   background-color: #171621;
 }
 </style>
